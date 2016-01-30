46880733e7f811338e06f6f1d5c2f86ce74afd3a
André Pinto <andre.pinto@accenture.com>
2016-01-30 17:29:52 -0200

    Initial Commit

---
+++ TraktDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TraktDemo
+//
+//  Created by André Pinto on 30/01/16.
+//  Copyright © 2016 André Pinto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
